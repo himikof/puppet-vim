f27dbee56ab6722b96bcda9c79efd1d859b1fcd5
Nikita Ofitserov <Nikita.Ofitserov@transas.com>
2012-08-07 21:52:18 +0400

    Add simple vim module

---
+++ manifests/init.pp
@@ -0,0 +1,19 @@
+import 'stdlib'
+import 'os_gentoo'
+
+# Class vim
+#
+#  Provides the vim editor configuration.
+#
+class vim {
+  
+  package { 'vim':
+    ensure   => 'installed',
+    require  => Package['portage'],
+  }
+
+  file { '/etc/vim/vimrc.local':
+    content => template("vim/vimrc"),
+    require => Package['vim'],
+  }
+}
